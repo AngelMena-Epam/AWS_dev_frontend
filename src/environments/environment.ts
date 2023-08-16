0d920bb63ddfe594f450d14e71faf40509ca4743
Angel Mena <angel_mena@epam.com>
2023-08-15 19:04:46 -0600

    feat: adding new frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://iaa11gfx2f.execute-api.us-east-1.amazonaws.com/dev',
+    bff: 'https://m5wf6665sb.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
